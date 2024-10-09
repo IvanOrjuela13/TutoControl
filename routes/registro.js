90d6fec3d289e607491ef697039b26d5b90d8318
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2024-10-08 23:34:59 -0500

    Update registro.js

--- routes/registro.js
+++ routes/registro.js
@@ -4,10 +4,15 @@ const router = express.Router();
 
 // Ruta para registrar entrada
 router.post('/entrada', async (req, res) => {
-    const { userId, deviceID, timestamp, location } = req.body;
+    const { userId, deviceID, ubicacion } = req.body;
 
     try {
-        const nuevoRegistro = new Registro({ userId, deviceID, timestamp, location, type: 'entrada' });
+        const nuevoRegistro = new Registro({ 
+            userId, 
+            deviceID, 
+            ubicacion, // Asegúrate de que este objeto tenga lat y lng
+            tipo: 'entrada' 
+        });
         await nuevoRegistro.save();
         res.status(201).json({ msg: 'Entrada registrada exitosamente' });
     } catch (error) {
@@ -18,10 +23,15 @@ router.post('/entrada', async (req, res) => {
 
 // Ruta para registrar salida
 router.post('/salida', async (req, res) => {
-    const { userId, deviceID, timestamp, location } = req.body;
+    const { userId, deviceID, ubicacion } = req.body;
 
     try {
-        const nuevoRegistro = new Registro({ userId, deviceID, timestamp, location, type: 'salida' });
+        const nuevoRegistro = new Registro({ 
+            userId, 
+            deviceID, 
+            ubicacion, // Asegúrate de que este objeto tenga lat y lng
+            tipo: 'salida' 
+        });
         await nuevoRegistro.save();
         res.status(201).json({ msg: 'Salida registrada exitosamente' });
     } catch (error) {
@@ -31,7 +41,3 @@ router.post('/salida', async (req, res) => {
 });
 
 module.exports = router;
-
-
-
-
