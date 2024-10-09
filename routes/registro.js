8ba87ad8ba1481e0a0afbbd099e20886e7e6b46e
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2024-10-08 23:25:34 -0500

    Update registro.js

--- routes/registro.js
+++ routes/registro.js
@@ -4,50 +4,34 @@ const router = express.Router();
 
 // Ruta para registrar entrada
 router.post('/entrada', async (req, res) => {
-    const { userId, deviceID, ubicacion } = req.body; // Cambiar 'location' por 'ubicacion'
-
-    // Verificar que se reciban los datos necesarios
-    if (!userId || !deviceID || !ubicacion) {
-        return res.status(400).json({ msg: 'Faltan datos requeridos' });
-    }
+    const { userId, deviceID, timestamp, location } = req.body;
 
     try {
-        const nuevoRegistro = new Registro({
-            userId,
-            deviceID, // Almacena el deviceID
-            tipo: 'entrada',
-            ubicacion, // { lat: ..., lng: ... }  asegúrate de que esto sea un objeto
-        });
+        const nuevoRegistro = new Registro({ userId, deviceID, timestamp, location, type: 'entrada' });
         await nuevoRegistro.save();
         res.status(201).json({ msg: 'Entrada registrada exitosamente' });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ msg: 'Error al registrar la entrada', error });
+        res.status(500).json({ msg: 'Error al registrar la entrada' });
     }
 });
 
 // Ruta para registrar salida
 router.post('/salida', async (req, res) => {
-    const { userId, deviceID, ubicacion } = req.body; // Cambiar 'location' por 'ubicacion'
-
-    // Verificar que se reciban los datos necesarios
-    if (!userId || !deviceID || !ubicacion) {
-        return res.status(400).json({ msg: 'Faltan datos requeridos' });
-    }
+    const { userId, deviceID, timestamp, location } = req.body;
 
     try {
-        const nuevoRegistro = new Registro({
-            userId,
-            deviceID, // Almacena el deviceID
-            tipo: 'salida',
-            ubicacion, // { lat: ..., lng: ... }  asegúrate de que esto sea un objeto
-        });
+        const nuevoRegistro = new Registro({ userId, deviceID, timestamp, location, type: 'salida' });
         await nuevoRegistro.save();
         res.status(201).json({ msg: 'Salida registrada exitosamente' });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ msg: 'Error al registrar la salida', error });
+        res.status(500).json({ msg: 'Error al registrar la salida' });
     }
 });
 
 module.exports = router;
+
+
+
+
