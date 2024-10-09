b7318918660e64fd04e0c0c42cef167a0f314de9
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2024-10-08 22:34:37 -0500

    Update registro.js

--- models/registro.js
+++ models/registro.js
@@ -4,47 +4,30 @@ const router = express.Router();
 
 // Ruta para registrar entrada
 router.post('/entrada', async (req, res) => {
-    const { userId, deviceID, ubicacion } = req.body;
-
-    // Generar timestamp con la fecha y hora actual
-    const timestamp = new Date(); 
+    const { userId, ubicacion, fecha } = req.body; // Se recibe el ID del usuario, ubicación y fecha
 
     try {
-        const nuevoRegistro = new Registro({ 
-            userId, 
-            tipo: 'entrada', 
-            ubicacion, 
-            fecha: timestamp // Se almacena la fecha actual
-        });
-        await nuevoRegistro.save();
-        res.status(201).json({ msg: 'Entrada registrada exitosamente' });
+        const nuevoRegistro = new Registro({ userId, tipo: 'entrada', ubicacion, fecha }); // Tipo es 'entrada'
+        await nuevoRegistro.save(); // Guardar el registro en la base de datos
+        res.status(201).json({ msg: 'Entrada registrada exitosamente' }); // Respuesta exitosa
     } catch (error) {
         console.error(error);
-        res.status(500).json({ msg: 'Error al registrar la entrada' });
+        res.status(500).json({ msg: 'Error al registrar la entrada' }); // Respuesta de error
     }
 });
 
 // Ruta para registrar salida
 router.post('/salida', async (req, res) => {
-    const { userId, deviceID, ubicacion } = req.body;
-
-    // Generar timestamp con la fecha y hora actual
-    const timestamp = new Date(); 
+    const { userId, ubicacion, fecha } = req.body; // Se recibe el ID del usuario, ubicación y fecha
 
     try {
-        const nuevoRegistro = new Registro({ 
-            userId, 
-            tipo: 'salida', 
-            ubicacion, 
-            fecha: timestamp // Se almacena la fecha actual
-        });
-        await nuevoRegistro.save();
-        res.status(201).json({ msg: 'Salida registrada exitosamente' });
+        const nuevoRegistro = new Registro({ userId, tipo: 'salida', ubicacion, fecha }); // Tipo es 'salida'
+        await nuevoRegistro.save(); // Guardar el registro en la base de datos
+        res.status(201).json({ msg: 'Salida registrada exitosamente' }); // Respuesta exitosa
     } catch (error) {
         console.error(error);
-        res.status(500).json({ msg: 'Error al registrar la salida' });
+        res.status(500).json({ msg: 'Error al registrar la salida' }); // Respuesta de error
     }
 });
 
-module.exports = router;
-
+module.exports = router; // Exportar el router para que pueda ser utilizado en otras partes de la aplicación
